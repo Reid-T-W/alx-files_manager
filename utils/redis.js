e98898a374df78e7ca90b345db7768188c2e7359
Teepee78 <eniolatoluwalase78@gmail.com>
2023-03-31 13:56:08 +0100

    Remove client.connect()

--- utils/redis.js
+++ utils/redis.js
@@ -1,31 +1,30 @@
-import { createClient } from 'redis';
+import { createClient } from "redis";
 class RedisClient {
-    constructor() {
-        this.connected = false;
-        const client = createClient();
-        client.on('error', err => console.log(err));
-        client.on('ready', () => this.connected = true);
-        client.connect();
+	constructor() {
+		this.connected = false;
+		const client = createClient();
+		client.on("error", (err) => console.log(err));
+		client.on("ready", () => (this.connected = true));
 
-        this.client = client
-    }
+		this.client = client;
+	}
 
-    isAlive() {
-        return this.connected;
-    }
+	isAlive() {
+		return this.connected;
+	}
 
-    async get(key) {
-        const value = await this.client.get(key);
-        return value;
-    }
+	async get(key) {
+		const value = await this.client.get(key);
+		return value;
+	}
 
-    async set(key, value, duration) {
-        await this.client.set(key, value, 'EX', duration);
-    }
+	async set(key, value, duration) {
+		await this.client.set(key, value, "EX", duration);
+	}
 
-    async del(key) {
-        await this.client.del(key);
-    }
+	async del(key) {
+		await this.client.del(key);
+	}
 }
 
 const redisClient = new RedisClient();
