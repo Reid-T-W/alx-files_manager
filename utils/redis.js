404643035c458d1fe3657ae17909367e7ea23011
Reid-T-W <rediettadesse100@gmail.com>
2023-03-31 12:52:27 +0300

    Task 0 partially compelete, with redis connect issues to fix

---
+++ utils/redis.js
@@ -0,0 +1,32 @@
+import { createClient } from 'redis';
+class RedisClient {
+    constructor() {
+        this.connected = false;
+        const client = createClient();
+        client.on('error', err => console.log(err));
+        client.on('ready', () => this.connected = true);
+        client.connect();
+
+        this.client = client
+    }
+
+    isAlive() {
+        return this.connected;
+    }
+
+    async get(key) {
+        const value = await this.client.get(key);
+        return value;
+    }
+
+    async set(key, value, duration) {
+        await this.client.set(key, value, 'EX', duration);
+    }
+
+    async del(key) {
+        await this.client.del(key);
+    }
+}
+
+const redisClient = new RedisClient();
+module.exports = redisClient;
