4d647bb4fda4ffbc4a52c4566919b1de190337b9
Reid-T-W <rediettadesse100@gmail.com>
2023-04-03 02:02:25 -0700

    db.js requiring modifications in isalive

--- utils/db.js
+++ utils/db.js
@@ -4,33 +4,51 @@ class DBClient {
   constructor(host = 'localhost', port = 27017, database = 'files_manager') {
     // Connection URL
     const url = `mongodb://${host}:${port}`;
-    const client = new MongoClient(url);
-
+    this.client = new MongoClient(url);
+    this.client.connect();
     // Database Name
-    const dbName = database;
-    try {
-      client.connect();
-    } catch (err) {
-      this.connected = false;
-    }
+    // const dbName = database;
+    // try {
+    //  client.connect();
+    // } catch (err) {
+    //  this.connected = false;
+    //}
 
-    this.db = client.db(dbName);
+    // this.db = client.db(dbName);
   }
 
   isAlive() {
-    return this.connected;
-  }
+    const connectPromise = new Promise((resolve, reject) => {
+      try {
+	console.log("In try")
+        this.client.connect();
+        resolve()
+      } catch(err) {
+        reject(err)
+      }
+    })
+    connectPromise.then(() => {
+      console.log("In here")
+      this.connected = true
+      return Promise.resolve(this.connected);
+    }).catch((err) => {
+      console.log("In catch")
+      this.connected = false
+      return Promise.reject(this.connected);
+    })
+    }
+  
 
   async nbUsers() {
     const collection = db.collection('users');
     const findResult = await collection.find({}).toArray();
-    return (length(findResult));
+    return (findResult.length);
   }
 
   async nbFiles() {
     const collection = db.collection('files');
     const findResult = await collection.find({}).toArray();
-    return (length(findResult));
+    return (findResult.length);
   }
 }
 const dbClient = new DBClient();
