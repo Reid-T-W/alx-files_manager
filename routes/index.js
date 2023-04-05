694bdbbf09dfd66058cfcc5e74a2c02251441526
Reid-T-W <rediettadesse100@gmail.com>
2023-04-05 08:44:19 +0300

    router updated to include publish and unpublish path

--- routes/index.js
+++ routes/index.js
@@ -22,6 +22,10 @@ router.post('/files', FilesController.postUpload);
 
 router.get('/files/:id', FilesController.getShow);
 
+router.put('/files/:id/publish', FilesController.putPublish);
+
+router.put('/files/:id/unpublish', FilesController.putUnpublish);
+
 router.get('/files', FilesController.getIndex);
 
 router.get('/files/:id/data', FilesController.getFile);
